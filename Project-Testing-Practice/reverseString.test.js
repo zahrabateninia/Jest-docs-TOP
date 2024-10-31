3daf55b217be0f9170cf75139570171dbf131db2
zahrabateninia <zahrabatenin@gmail.com>
2024-10-31 16:18:54 +0330

    Add test cases and edge cases of reverseString function

--- Project-Testing-Practice/reverseString.test.js
+++ Project-Testing-Practice/reverseString.test.js
@@ -1,7 +1,31 @@
 import { reverseString } from "./reverseString";
 
-test('abc should be cba', () =>{
+test('reverses a simple word', () =>{
     let expected = 'cba';
     let actual = 'abc'
+    expect(reverseString(actual)).toBe(expected)
+})
+
+test('reverses a single character', () =>{
+    let expected = 'a';
+    let actual = 'a';
+    expect(reverseString(actual)).toBe(expected)
+})
+
+test('reverses an empty string', () =>{
+    expect(reverseString('')).toBe('')
+})
+
+// Edge Cases
+
+test('reverses a string with spaces', () =>{
+    let expected = 'hello world';
+    let actual = 'dlrow olleh';
+    expect(reverseString(actual)).toBe(expected)
+})
+
+test('reverses a string with special characters', () =>{
+    let expected = 'hello!';
+    let actual = '!olleh';
     expect(reverseString(actual)).toBe(expected)
 })
