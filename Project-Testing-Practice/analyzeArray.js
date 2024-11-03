13cede4b4882318b3f068a8d143591d0c74e278c
zahrabateninia <zahrabatenin@gmail.com>
2024-11-03 21:24:47 +0330

    Calculate the average of array elements

---
+++ Project-Testing-Practice/analyzeArray.js
@@ -0,0 +1,19 @@
+// An analyzeArray function that takes an array of numbers and returns an object with the following properties: average, min, max, and length.
+
+const analyzeArray = (arr) => {
+    let result = {}
+    let arrLen = arr.length
+
+    // Calculate the average of numbers in the array
+    let sum = 0
+    arr.forEach(num => {
+        sum += num;
+    })
+    let sumOfArrayElements  = sum / arrLen;
+    
+    // Calculate the min element of the array
+    
+
+
+
+}
